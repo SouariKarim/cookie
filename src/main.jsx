7f052e874b62e37e47e750e811ea4a230ae1bbae
SOUARI KARIM <EXT_KSO19@open-groupe.com>
2023-04-24 11:05:40 +0200

    change script

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Cookiebot } from 'react-cookiebot';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Cookiebot id="3d0c579f-4ac6-48e0-8c20-7a50f1596e8c" />
+  {/* <Cookiebot id="3d0c579f-4ac6-48e0-8c20-7a50f1596e8c" /> */}
 
     <App />
   </React.StrictMode>,
