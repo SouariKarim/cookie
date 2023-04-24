84a347f0545e40a93bd63292eccd93a76c2bfbfb
SOUARI KARIM <EXT_KSO19@open-groupe.com>
2023-04-24 15:18:49 +0200

    intial

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Cookiebot } from 'react-cookiebot';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  {/* <Cookiebot id="3d0c579f-4ac6-48e0-8c20-7a50f1596e8c" /> */}
-
     <App />
   </React.StrictMode>,
 )
